af8efd552a737b867e0228d0b64ea87481754dfd
evagolabek <golabek.eva@gmail.com>
2019-01-05 19:17:31 +0100

    passing the element/property of one component down to another component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  //js syntax, key-value pairs as elements array
+  serverElements = [{type: 'server', name: 'Testserver', content: 'just a test'}];
   
 }
